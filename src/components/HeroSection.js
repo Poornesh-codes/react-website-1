7c9da501bf3c57aaed7fb1f6f7869a3d2bb4f74c
Poornesh-codes <poorneshk9@gmail.com>
2026-03-19 17:41:42 +0530

    added navbar css files added button , HeroSection,routes

---
+++ src/components/HeroSection.js
@@ -0,0 +1,29 @@
+import '../App.css'
+import { Button } from './button'
+import './HeroSection.css'
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+        <video src="/videos/video-2.mp4" autoPlay loop muted />
+        <h1>ADVENTURE AWAITS</h1>
+        <p>what are u waiting for?</p>
+        <div className="hero-btns">
+            <Button className='btns' buttonStyle='btn--outline'
+            buttonSize='btn--large'
+            >
+                GET STARTED 
+            </Button>
+            <Button 
+            className='btns' 
+            buttonStyle='btn--primary'
+            buttonSize='btn--large'
+            >
+                watch trailer <i className='far fa-play-circle'/> 
+            </Button>
+            
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
